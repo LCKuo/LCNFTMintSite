e4543fe1391d800f4be99266d5e9f13e4d3ef2b5
LCKuo <asdfg6311@gmail.com>
2022-03-11 09:40:33 +0800

    Updates

---
+++ precache-manifest.95b717f7763e22a47d04a00579b15671.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "89267e53a07d8333a3e0",
+    "url": "/LCNFTMintSite/static/css/main.a114b701.chunk.css"
+  },
+  {
+    "revision": "89267e53a07d8333a3e0",
+    "url": "/LCNFTMintSite/static/js/main.89267e53.chunk.js"
+  },
+  {
+    "revision": "42577204aa4aa8d26624",
+    "url": "/LCNFTMintSite/static/css/1.7d1edf24.chunk.css"
+  },
+  {
+    "revision": "42577204aa4aa8d26624",
+    "url": "/LCNFTMintSite/static/js/1.42577204.chunk.js"
+  },
+  {
+    "revision": "82aed772078937f2cf0f",
+    "url": "/LCNFTMintSite/static/js/runtime~main.82aed772.js"
+  },
+  {
+    "revision": "9697bb334465e28ca8b9f9dbefec2a96",
+    "url": "/LCNFTMintSite/static/media/LC.9697bb33.png"
+  },
+  {
+    "revision": "76e673b3098681856cf86daa73031ce9",
+    "url": "/LCNFTMintSite/static/media/eth.76e673b3.png"
+  },
+  {
+    "revision": "819d8fe14f1f06d2f5d68c00ef0ba979",
+    "url": "/LCNFTMintSite/static/media/logo.819d8fe1.png"
+  },
+  {
+    "revision": "1b5359ccb77999d2f68fcbff6b31211b",
+    "url": "/LCNFTMintSite/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/LCNFTMintSite/precache-manifest.95b717f7763e22a47d04a00579b15671.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/LCNFTMintSite/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
